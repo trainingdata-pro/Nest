7e0025b1e0b1443c6ba6d8c80dfb17d6da7bb3ce
DmBand <d.bandysik@gmail.com>
2023-07-24 18:20:19 +0300

    feat: update main methods

--- backend/src/projects/api.py
+++ backend/src/projects/api.py
@@ -38,6 +38,27 @@ class ProjectAPIViewSet(BaseAPIViewSet):
     filterset_class = ProjectFilter
     ordering_fields = ['name', 'manager', 'assessors_count', 'status', 'date_of_creation']
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        project = serializer.save()
+        response = serializers.ProjectSerializer(project)
+
+        return Response(response.data, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        project = serializer.save()
+        response = serializers.ProjectSerializer(project)
+
+        return Response(response.data, status=status.HTTP_200_OK)
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.check_project(instance)
@@ -74,59 +95,3 @@ class ProjectAPIViewSet(BaseAPIViewSet):
                     .annotate(assessors_count=Count('assessors'))
                     .select_related('manager__user')
                     .order_by('manager__last_name', 'name', '-date_of_creation'))
-
-# @method_decorator(name='retrieve', decorator=project_schema.retrieve())
-# @method_decorator(name='list', decorator=project_schema.list())
-# @method_decorator(name='create', decorator=project_schema.create())
-# @method_decorator(name='partial_update', decorator=project_schema.partial_update())
-# @method_decorator(name='destroy', decorator=project_schema.destroy())
-# class ProjectAPIViewSet(BaseAPIViewSet):
-#     queryset = (Project.objects
-#                 .select_related('owner__user')
-#                 .order_by('owner__last_name', 'name', '-date_of_create'))
-#     permission_classes = {
-#         'create': (IsAuthenticated,),
-#         'partial_update': (IsAuthenticated, ProjectOwner),
-#         'retrieve': (IsAuthenticated,),
-#         'list': (IsAuthenticated,),
-#         'destroy': (IsAuthenticated, ProjectOwner),
-#     }
-#     serializer_class = {
-#         'create': serializers.CreateProjectSerializer,
-#         'partial_update': serializers.CreateProjectSerializer,
-#         'retrieve': serializers.ProjectSerializer,
-#         'list': serializers.ProjectSerializer
-#     }
-#     http_method_names = ['get', 'post', 'patch', 'delete']
-#     filterset_class = ProjectFilter
-#     ordering_fields = ['name', 'owner', 'date_of_create']
-#
-#     def get_serializer_class(self):
-#         return self.serializer_class[self.action]
-#
-#     def get_permissions(self):
-#         action = self.action
-#         if action not in self.permission_classes:
-#             raise MethodNotAllowed(self.request.method)
-#         return [perm() for perm in self.permission_classes[action]]
-#
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         project = serializer.save()
-#         response = serializers.ProjectSerializer(project)
-#
-#         return Response(response.data, status=status.HTTP_201_CREATED)
-#
-#     def partial_update(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(
-#             instance,
-#             data=request.data,
-#             partial=True
-#         )
-#         serializer.is_valid(raise_exception=True)
-#         project = serializer.save()
-#         response = serializers.ProjectSerializer(project)
-#
-#         return Response(response.data, status=status.HTTP_200_OK)
