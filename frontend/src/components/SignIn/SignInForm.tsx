ff1f6edd61891b4253e85de3dd122dd34b6ef1ce
vaneuss1 <vanessem9914@gmail.com>
2023-07-25 18:19:09 +0300

    signup

--- frontend/src/components/SignIn/SignInForm.tsx
+++ frontend/src/components/SignIn/SignInForm.tsx
@@ -1,7 +1,7 @@
 
 import {useForm} from "react-hook-form";
-import {useState} from "react";
-import {redirect, useLocation, useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {NavLink, redirect, useLocation, useNavigate} from "react-router-dom";
 import {useContext} from "react";
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
@@ -11,38 +11,36 @@ interface ISignIn {
 }
 const SignInForm = () => {
     const navigate = useNavigate();
-    const {register, getValues, handleSubmit} = useForm<ISignIn>()
+    const {register,formState:{errors},  getValues, handleSubmit} = useForm<ISignIn>()
+    const [serverError, setServerError] = useState<string>('')
     const [isLoading, setIsLoading] = useState(false)
-    const [serverError, setServerError] = useState<string | null>(null)
     const {store} = useContext(Context)
+    useEffect(()=> {
+
+    }, [serverError, isLoading])
     const onSubmit = async () => {
         setIsLoading(true)
         const values = getValues()
-        try {
-            await store.login(values.username, values.password)
-            navigate("/dashboard/team") // TODO: redirect to main page
-
-        } catch (err) {
-            // TODO: maybe improve error handling
-            setServerError('Не удалось войти в аккаунт, проверьте правильность введенных вами данных')
-        } finally {
-            setIsLoading(false)
-        }
+            await store.login(values.username, values.password).
+            then(() => {
+                navigate('/dashboard/main')
+            }).catch(err => setServerError(err.response.data.detail)).finally(() => setIsLoading(false))
     }
     return (
-        <form className="w-[30rem] space-y-8"
+        <form className="w-[30rem]"
               onSubmit={handleSubmit(onSubmit)}>
-            <div className="space-y-2">
+            <div className="my-4">
                 <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="username">
                     Имя пользователя
                 </label>
                 <input
                     className="flex h-10 rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full"
                     placeholder="username"
-                    {...register("username")}
+                    {...register("username", {required: "Обязательное поле"})}
                 />
+                <p className='h-6 text-red-500 text-sm pt-1'>{errors.username && errors.username?.message}</p>
             </div>
-            <div className="space-y-2">
+            <div className="my-4">
                 <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor="password">
                     Пароль
                 </label>
@@ -50,29 +48,27 @@ const SignInForm = () => {
                     className="flex h-10 rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full"
                     type="password"
                     placeholder="Введите ваш пароль"
-                    {...register("password")}
+                    {...register("password", {required: "Обязательное поле"})}
                 />
+                <p className='h-6 text-red-500 text-sm pt-1'>{errors.password && errors.password?.message}</p>
             </div>
-            <section className="flex items-center justify-between">
-                <div className="space-y-2">
-                    <div className="text-sm text-gray-500">Не зарегистрированы?<a
-                        className="cursor-pointer pl-1 text-black text-primary hover:underline" href="/signup">Создайте аккаунт</a>
+            <p className="text-sm text-red-500 h-5">{serverError}</p>
+            <section className="flex justify-between items-center">
+                <div className="w-100">
+                    <div className="text-sm text-gray-500">Не зарегистрированы?<NavLink
+                        className="cursor-pointer pl-1 text-black text-primary hover:underline" to="/register">Создайте аккаунт</NavLink>
                     </div>
                 </div>
-                {isLoading ? <div className="pr-7 pl-3 px-4"><div
-                        className="inline-block  h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                        role="status">
-  <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-  >Loading...</span>
-                    </div></div>:
-                <button
-                    className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-10 py-2 px-4"
-                    type="submit">Войти
-                </button>}
+                <div className="">
+                    <button disabled={isLoading}
+                            className="bg-black text-white rounded-md text-sm font-medium disabled:opacity-50 transition-colors hover:bg-primary/90 h-10 py-2 px-4"
+                            type="submit">{!isLoading ? "Войти" : <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-white"/>}
+                    </button>
+                </div>
+
             </section>
         </form>
     );
 };
 
-export default SignInForm;
+export default observer(SignInForm);
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,11 +2,6 @@ import {makeAutoObservable} from "mobx";
 import AuthService from "../services/AuthService";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-import {IManager} from "../models/ManagerResponse";
-import ProjectsService from "../services/ProjectsService";
-import ManagerService from "../services/ManagerService";
-import {IAssessor} from "../models/AssessorResponse";
-import AssessorsService from "../services/AssessorsService";
 import {API_URL} from "../http";
 
 interface UserData{
@@ -51,75 +46,18 @@ export default class Store {
     isAuth = false
     manager = {} as UserData
     managerData = {} as ManagerData
-
-
-    assessors = [] as IAssessor[]
     isLoading = false
-    projects = [] as Project[]
-    selectedRow = null
-    selectedRowAssessors = null
-    currentProjectManager = {} as IManager
+
 
     constructor() {
         makeAutoObservable(this)
-    }
-
-    setSelectedRow(selectedRow: any) {
-        this.selectedRow = selectedRow
-    }
-    setSelectedRowAssessors(selectedRow: any) {
-        this.selectedRowAssessors = selectedRow
     }
 
     setIsLoading(bool: boolean) {
         this.isLoading = bool
     }
 
-    setCurrentProjectManager(manager:IManager){
-        this.currentProjectManager = manager
-    }
-    setCurrentProjectManager1(id: number) {
-        ManagerService.fetch_manager(id).then(res => {
-            this.setCurrentProjectManager(res.data)
-            console.log(this.currentProjectManager)
-        })
-    }
 
-    setProjects(projects: any) {
-        this.projects = projects
-    }
-    setAssessors(assessors:any){
-        this.assessors = assessors
-    }
-
-    fetchProjects(managerId: number) {
-        // @ts-ignore
-        ProjectsService.fetchManagerProjects(managerId).then(res => this.setProjects(res.data.results))
-    }
-    fetchAssessors() {
-        // @ts-ignore
-        AssessorsService.fetchAssessors().then(res => this.setAssessors(res.data.results))
-    }
-
-    async addProject(values: any) {
-        return await ProjectsService.addProjects(values)
-    }
-    async addAssessor(values: any) {
-        return await AssessorsService.addAssessors(values)
-    }
-
-    async deleteProject(ids: number[]) {
-        // @ts-ignore
-        ids.map(k => {
-            ProjectsService.deleteProjects(k).then(() => this.setProjects([...this.projects.filter(res => res.id !== k)]))
-        })
-    }
-    async deleteAssessors() {
-        // @ts-ignore
-        this.selectedRowAssessors.map(k => {
-            AssessorsService.deleteAssessors(k).then(() => this.setAssessors([...this.assessors.filter(res => res.id !== k)]))
-        })
-    }
 
     setAuth(bool: boolean) {
         this.isAuth = bool
@@ -131,9 +69,7 @@ export default class Store {
     setManagerData(manager: ManagerData) {
         this.managerData = manager
     }
-    getProjectCount(id:number){
-        return ProjectsService.getProjectsAssessorsCount(id).then(res => res.data.count)
-    }
+
     async login(username: string, password: string) {
         this.setIsLoading(true)
         const response = await AuthService.login(username, password)
@@ -141,7 +77,6 @@ export default class Store {
         const decodeJwt: Token = jwtDecode(response.data.access)
         this.setManager(decodeJwt.user_data)
         document.cookie = `refresh=${response.data.refresh}`
-        await ManagerService.fetch_manager(decodeJwt.user_data.manager_id).then(res => this.setManagerData(res.data))
         this.setAuth(true)
         this.setIsLoading(false)
     }
@@ -156,7 +91,6 @@ export default class Store {
             localStorage.setItem('token', response.data.access)
             const decodeJwt: Token = jwtDecode(response.data.access)
             this.setManager(decodeJwt.user_data)
-            await ManagerService.fetch_manager(decodeJwt.user_data.manager_id).then(res => this.setManagerData(res.data))
             this.setAuth(true)
         } catch (e: any) {
             this.setAuth(false)
