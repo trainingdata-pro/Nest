1a57b648474fa9ddf9278fda27880111171566e5
DmBand <d.bandysik@gmail.com>
2023-07-27 14:31:56 +0300

    feat: add new permissions

--- backend/src/core/utils/permissions.py
+++ backend/src/core/utils/permissions.py
@@ -1,21 +1,33 @@
 from rest_framework.permissions import BasePermission
 
 
-class ProjectOwnerOrTeamLead(BasePermission):
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return hasattr(request.user, 'manager') or request.user.is_superuser
+
+
+class ProjectPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.manager in obj.manager.all() or \
+            request.user.manager.is_operational_manager
+
+
+class AssessorPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.manager.pk == obj.manager.pk or \
-            obj.owner.operational_manager.pk == request.user.manager.pk
+            obj.manager.operational_manager == request.user.manager
 
 
-class AssessorOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.manager.pk == obj.manager.pk
-
-
-class OwnerOrSecondManager(BasePermission):
+class AssessorProjectPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.manager.pk == obj.manager.pk or \
+            obj.manager.operational_manager == request.user.manager or \
             obj.second_manager.filter(pk=request.user.manager.pk).exists()
+
+
+class WorkingHoursPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        pass
 
 
 class IsCurrentManager(BasePermission):
@@ -23,6 +35,6 @@ class IsCurrentManager(BasePermission):
         return request.user.manager.pk == obj.pk
 
 
-class IsOperationalManagerOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_superuser or request.user.manager.is_operational_manager
+# class IsOperationalManagerOrAdmin(BasePermission):
+#     def has_permission(self, request, view):
+#         return request.user.is_superuser or request.user.manager.is_operational_manager
