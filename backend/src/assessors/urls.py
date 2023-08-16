f4fc1842f475b09c4d477521f553cdf6fc340668
DmBand <d.bandysik@gmail.com>
2023-08-16 18:27:38 +0300

    feat: add ability to add assessor to free resources

--- backend/src/assessors/urls.py
+++ backend/src/assessors/urls.py
@@ -13,13 +13,13 @@ assessors_router.register('', api.AssessorAPIViewSet, basename='assessor')
 wh_router = routers.DefaultRouter()
 wh_router.register('', api.WorkingHoursAPIViewSet, basename='working-hours')
 
-# fr_router = routers.DefaultRouter()
-# fr_router.register('', api.FreeResourcesAPIViewSet, basename='free-resources')
+fr_router = routers.DefaultRouter()
+fr_router.register('', api.FreeResourcesAPIViewSet, basename='fr-get')
 
 urlpatterns = [
     path('assessors/check/', api.AssessorCheckAPIView.as_view()),
     path('skills/', include(skills_router.urls)),
     path('assessors/', include(assessors_router.urls)),
-    path('working_hours/', include(wh_router.urls))
-    # path('free_resources/', include(fr_router.urls))
+    path('working_hours/', include(wh_router.urls)),
+    path('free_resources/', include(fr_router.urls))
 ]
