e764e58aaa99d2292b3ddaadb00cb6a896908d08
Ivan <issemenov99@mail.ru>
2023-08-29 17:31:27 +0300

    'fix'

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -24,7 +24,9 @@ interface FormProps {
     operational_manager: SelectProps
 }
 
-const Profile = () => {
+const Profile = ({setIsOpen}: {
+    setIsOpen: React.Dispatch<boolean>
+}) => {
     const {store} = useContext(Context)
     useMemo(async () => {
 
@@ -33,7 +35,6 @@ const Profile = () => {
         })))
         if (store.managerData.operational_manager){
             await ManagerService.fetch_manager(store.managerData.operational_manager).then(res => {
-                console.log(res.data)
                 setValue('operational_manager', {
                     value: res.data.id,
                     label: `${res.data.last_name} ${res.data.first_name}`
@@ -63,7 +64,7 @@ const Profile = () => {
         store.managerData.operational_manager = newData.operational_manager
         ManagerService.patchManager(data.id, newData)
         ManagerService.patchBaseUser(store.user_id, {'username': getValues('username')}).then((res) => {
-            navigate(-1)
+            setIsOpen(false)
         })
     }
 
@@ -71,59 +72,45 @@ const Profile = () => {
         setValue('operational_manager', value);
     };
     return (
-        <div className="flex h-screen w-screen items-end justify-center sm:items-center">
-
-            <div className="bg-white fixed z-50 w-full max-w-max border bg-background p-6 opacity-100">
-
-                <form className="w-[40rem] space-y-4" onSubmit={handleSubmit(onSubmit)}>
-                    <section className="grid grid-cols-3 gap-x-3">
-                        <div className="col-span-1">
-                            <MyLabel required={true}>Фамилия</MyLabel>
-                            <MyInput register={{...register('last_name')}} type="text" placeholder="Фамилия"/>
-                        </div>
-                        <div className="col-span-1">
-                            <MyLabel required={true}>Имя</MyLabel>
-                            <MyInput register={{...register('first_name')}} type="text" placeholder="Имя"/>
-                        </div>
-                        <div className="col-span-1">
-                            <MyLabel required={true}>Отчество</MyLabel>
-                            <MyInput register={{...register('middle_name')}} type="text" placeholder="Отчество"/>
-                        </div>
-                    </section>
-                    <div>
-                        <MyLabel required={true}>Ник в ТГ</MyLabel>
-                        <MyInput register={{...register('username')}} type="text" placeholder="Ник в ТГ"/>
+        <div>
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+                <section className="">
+                    <div className="col-span-1">
+                        <MyLabel required={true}>Фамилия</MyLabel>
+                        <MyInput register={{...register('last_name')}} type="text" placeholder="Фамилия"/>
                     </div>
-                    <div>
-                        <MyLabel required={true}>Ответственный TeamLead</MyLabel>
-                        <Select
-                            isDisabled={store.managerData.is_operational_manager || !!store.managerData.operational_manager}
-                            options={operationsManagers}
-                            value={watch('operational_manager')}
-                            {...register('operational_manager', {required: 'Обязательное поле'})}
-                            onChange={handleSelectChange}
-                        />
-
+                    <div className="col-span-1">
+                        <MyLabel required={true}>Имя</MyLabel>
+                        <MyInput register={{...register('first_name')}} type="text" placeholder="Имя"/>
                     </div>
-                    <div>
-                        <MyLabel required={true}>ID</MyLabel>
-                        <MyInput register={{...register('id')}} type="text" placeholder="Фамилия" disabled={true}/>
+                    <div className="col-span-1">
+                        <MyLabel required={true}>Отчество</MyLabel>
+                        <MyInput register={{...register('middle_name')}} type="text" placeholder="Отчество"/>
                     </div>
-                    <button
-                        className="bg-black text-white rounded-md text-sm font-medium disabled:opacity-50 transition-colors hover:bg-primary/90 h-10 py-2 px-4">Сохранить
-                    </button>
-                </form>
-                <div className="mt-5 flex justify-between">
-                    <button
-                        className="bg-black text-white rounded-md text-sm font-medium disabled:opacity-50 transition-colors hover:bg-primary/90 h-10 py-2 px-4"
-                        onClick={() => {
-                            // store.setShowProfile(false)
-                            navigate(-1)
-                        }}>Закрыть страницу
-                    </button>
+                </section>
+                <div>
+                    <MyLabel required={true}>Ник в ТГ</MyLabel>
+                    <MyInput register={{...register('username')}} type="text" placeholder="Ник в ТГ"/>
+                </div>
+                <div>
+                    <MyLabel required={true}>Ответственный TeamLead</MyLabel>
+                    <Select
+                        isDisabled={store.managerData.is_operational_manager || !!store.managerData.operational_manager}
+                        options={operationsManagers}
+                        value={watch('operational_manager')}
+                        {...register('operational_manager', {required: 'Обязательное поле'})}
+                        onChange={handleSelectChange}
+                    />
 
                 </div>
-            </div>
+                <div>
+                    <MyLabel required={true}>ID</MyLabel>
+                    <MyInput register={{...register('id')}} type="text" placeholder="ID" disabled={true}/>
+                </div>
+                <button
+                    className="bg-black text-white rounded-md w-full text-sm font-medium disabled:opacity-50 transition-colors hover:bg-primary/90 h-10 py-2 px-4">Сохранить
+                </button>
+            </form>
         </div>
     )
         ;
