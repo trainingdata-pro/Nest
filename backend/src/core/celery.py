90c896ca287f57c8aad24619428f9b0380d10286
DmBand <d.bandysik@gmail.com>
2023-09-08 12:58:16 +0300

    chore: change celery config

--- backend/src/core/celery.py
+++ backend/src/core/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('app')
+app = Celery('app', broker_connection_retry_on_startup=True)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
