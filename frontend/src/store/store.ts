1f288c406aaeb20045d9d3bcf1809c64c0ffbcf6
Ivan <issemenov99@mail.ru>
2023-09-11 18:18:33 +0300

    refactor authentication

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -8,9 +8,11 @@ import Cookies from 'universal-cookie';
 interface UserData {
     is_active: boolean,
     is_admin: boolean,
-    is_operational_manager: boolean,
-    manager_id: number,
+    email: string,
+    status: string,
     username: string,
+    is_teamlead: boolean
+    teamlead: number
 
 
 }
@@ -22,24 +24,10 @@ export interface Token {
 
 }
 
-export interface ManagerData {
-    id: number,
-    user: {
-        id: number,
-        username: string,
-        email: string
-    },
-    last_name: string,
-    first_name: string,
-    middle_name: string,
-    is_operational_manager: boolean,
-    operational_manager: number | string,
-}
-
 export default class Store {
     user_id: number= 0
     isAuth = false
-    managerData = {} as ManagerData
+    managerData = {} as UserData
     isLoading = false
     showProfile = false
     cookies = new Cookies()
@@ -65,25 +53,27 @@ export default class Store {
     setUserId(id:number){
         this.user_id = id
     }
-    setManagerData(manager: ManagerData) {
+    setManagerData(manager: UserData) {
         this.managerData = manager
     }
 
     async login(email: string, password: string) {
         await AuthService.login(email, password)
             .then(res => {
+                console.log(res)
                 localStorage.setItem('token', res.data.access)
                 const decodeJwt: Token = jwtDecode(res.data.access)
                 this.cookies.set('refresh', res.data.refresh, {path: '/', maxAge: decodeJwt.exp})
-                const managerId = decodeJwt.user_data.manager_id
-                this.setUserId(decodeJwt.user_id)
-                ManagerService.fetch_manager(managerId).then(res => {
-                    this.setManagerData(res.data)
-                    const manager = res.data
-                    if (manager.first_name === '' || manager.last_name === '' || manager.middle_name === '' || !manager.is_operational_manager && manager.operational_manager === null){
-                        this.setShowProfile(true)
-                    }
-                })
+                console.log(decodeJwt)
+                const managerId = decodeJwt.user_id
+                this.setUserId(managerId)
+                // ManagerService.fetch_manager(managerId).then(res => {
+                //     this.setManagerData(res.data)
+                //     const manager = res.data
+                //     if (manager.first_name === '' || manager.last_name === '' || manager.middle_name === '' || !manager.is_operational_manager && manager.operational_manager === null){
+                //         this.setShowProfile(true)
+                //     }
+                // })
                 this.setAuth(true)
             })
             .catch(e => {
@@ -106,15 +96,15 @@ export default class Store {
             const response = await axios.post(`${API_URL}/api/token/refresh/`, {'refresh': this.cookies.get('refresh')})
             localStorage.setItem('token', response.data.access)
             const decodeJwt: Token = jwtDecode(response.data.access)
-            const managerId = decodeJwt.user_data.manager_id
+            const managerId = decodeJwt.user_id
             this.setUserId(decodeJwt.user_id)
-            await ManagerService.fetch_manager(managerId).then(res => {
-                this.setManagerData(res.data)
-                const manager = res.data
-                if (manager.first_name === '' || manager.last_name === '' || manager.middle_name === '' || !manager.is_operational_manager && manager.operational_manager === null){
-                    this.setShowProfile(true)
-                }
-            })
+            // await ManagerService.fetch_manager(managerId).then(res => {
+            //     this.setManagerData(res.data.)
+            //     const manager = res.data
+            //     if (manager.first_name === '' || manager.last_name === '' || manager.middle_name === '' || !manager.is_operational_manager && manager.operational_manager === null){
+            //         this.setShowProfile(true)
+            //     }
+            // })
             this.setAuth(true)
         } catch (e: any) {
             this.setAuth(false)
