ed62976b036c553ba19a617cf304ed069207a5e5
DmBand <d.bandysik@gmail.com>
2023-09-12 21:09:15 +0300

    refactor: move get_code() to another module

--- backend/src/core/utils/common.py
+++ backend/src/core/utils/common.py
@@ -1,4 +1,13 @@
 import datetime
+import hashlib
+import time
+
+
+def get_code() -> str:
+    """ Get 20 character long code """
+    h = hashlib.sha512()
+    h.update(str(time.time()).encode('utf-8'))
+    return h.hexdigest()[:20]
 
 
 def current_date() -> datetime.date:
