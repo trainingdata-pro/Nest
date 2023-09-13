291c2ee223280e27a50258a954a528fd39aa9398
DmBand <d.bandysik@gmail.com>
2023-09-13 22:01:55 +0300

    chore: fix deploy config

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -143,8 +143,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'api/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'api/static/')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
