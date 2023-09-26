34f4027ae54e3bb0151c81e120832803c7007b8d
vaneuss1 <vanessem9914@gmail.com>
2023-09-26 14:46:58 +0300

    add login and password popup

--- frontend/src/components/Assessors/LoginAndPassword.tsx
+++ frontend/src/components/Assessors/LoginAndPassword.tsx
@@ -11,7 +11,7 @@ import {observer} from "mobx-react-lite";
 
 // @ts-ignore
 const TableCell = observer(({getValue, row, column, table}) => {
-    const {store}  = useContext(Context)
+    const {store} = useContext(Context)
     const initialValue = getValue();
     const [value, setValue] = useState(initialValue);
     useEffect(() => {
@@ -19,15 +19,16 @@ const TableCell = observer(({getValue, row, column, table}) => {
     }, [initialValue]);
     const onBlur = () => {
         table.options.meta?.updateData(row.index, column.id, value);
-        if (row.original.tool !== value){
-        AssessorService.patchCredentials(row.original.id, {
-            assessor: row.original.assessor.id,
-            [column.id] : value
-        }).then((res) => console.log(res.data))}
+        if (row.original.tool !== value) {
+            AssessorService.patchCredentials(row.original.id, {
+                assessor: row.original.assessor.id,
+                [column.id]: value
+            }).then((res) => console.log(res.data))
+        }
     };
     return (
         <textarea
-            className={`text-center resize-none ${!store.isEditableLoginAndPassword ? "opacity-50":""}`}
+            className={`text-center resize-none ${!store.isEditableLoginAndPassword ? "opacity-50" : ""}`}
             value={value}
             disabled={!store.isEditableLoginAndPassword}
             onChange={(e) => setValue(e.target.value)}
@@ -46,7 +47,7 @@ const columns = [
         cell: TableCell,
     }),
     columnHelper.accessor("password", {
-        header: "Password",
+        header: "Пароль",
         cell: TableCell,
     }),
 ];
@@ -80,38 +81,41 @@ const TableLog = () => {
         },
     });
     return (
-        <div className="rounded-t-[20px] border border-b-gray-400 bg-white">
-            <table className="w-full">
-                <thead className="">
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => (
-                            <th key={header.id} className="bg-[#E7EAFF]">
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-                </thead>
-                <tbody>
-                {table.getRowModel().rows.map((row) => (
-                    <tr key={row.id}>
-                        {row.getVisibleCells().map((cell) => (
-                            <td key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            <button onClick={() => store.setEditableLoginAndPassword(!store.isEditableLoginAndPassword)}>{store.isEditableLoginAndPassword? "Сохранить": "Редактировать"}</button>
-        </div>
+        <>
+            <div className="rounded-t-[20px] border border-b-gray-400 bg-white">
+                <table className="w-full">
+                    <thead className="">
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th key={header.id} className="bg-[#E7EAFF] px-[15px] py-[10px]">
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                    </thead>
+                    <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+            <button
+                onClick={() => store.setEditableLoginAndPassword(!store.isEditableLoginAndPassword)}>{store.isEditableLoginAndPassword ? "Сохранить" : "Редактировать"}</button>
+        </>
     );
 };
 
