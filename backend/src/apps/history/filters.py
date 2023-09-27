e7f9ffc326b72788fdf201d982c852af24b616f9
DmBand <d.bandysik@gmail.com>
2023-09-27 12:31:33 +0300

    feat: add history filtering and ordering

--- backend/src/apps/history/filters.py
+++ backend/src/apps/history/filters.py
@@ -4,8 +4,9 @@ from .models import History
 
 
 class HistoryFilter(filters.FilterSet):
-    event = filters.CharFilter(lookup_expr='iexact')
+    action = filters.CharFilter(lookup_expr='iexact')
+    attribute = filters.CharFilter(lookup_expr='iexact')
 
     class Meta:
         model = History
-        fields = ['event']
+        fields = ['action', 'attribute']
