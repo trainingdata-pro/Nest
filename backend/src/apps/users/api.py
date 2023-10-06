0ac158ae5116f8b20bb65a71ee74126ea666bbdf
DmBand <d.bandysik@gmail.com>
2023-10-06 17:01:22 +0300

    fix: fix permission to manager profile

--- backend/src/apps/users/api.py
+++ backend/src/apps/users/api.py
@@ -69,6 +69,7 @@ class ManagerAPIViewSet(BaseAPIViewSet):
         'list': (IsAuthenticated,),
         'partial_update': (
             IsAuthenticated,
+            permissions.IsManager,
             permissions.IsCurrentManager
         )
     }
