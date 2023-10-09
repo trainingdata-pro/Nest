c73958aae672c75afc20e009c44b34baa9cf47fc
Ivan <issemenov99@mail.ru>
2023-10-09 17:06:13 +0300

    add notification window

--- frontend/src/components/UI/Notify.tsx
+++ frontend/src/components/UI/Notify.tsx
@@ -1,22 +1,10 @@
-
 import {toast} from "react-toastify";
-
-export const errorNotification = (msg:string) => toast.error(msg, {
+const options = {
     position: toast.POSITION.BOTTOM_LEFT,
     autoClose: 2000,
     pauseOnHover: false,
     draggable: true,
-});
-export const successNotification = (msg:string) => toast.success(msg, {
-    position: toast.POSITION.BOTTOM_LEFT,
-    autoClose: 2000,
-    pauseOnHover: false,
-    draggable: true,
-});
-export const warnNotification = (msg:string) => toast.warn(msg, {
-    position: toast.POSITION.BOTTOM_LEFT,
-    autoClose: 2000,
-    pauseOnHover: false,
-    draggable: true,
-});
-
+}
+export const errorNotification = (msg:string) => toast.error(msg, options);
+export const successNotification = (msg:string) => toast.success(msg, options);
+export const warnNotification = (msg:string) => toast.warn(msg, options);
