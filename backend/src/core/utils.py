9057819be4da250748a212c46ce4bf49096e2250
DmBand <d.bandysik@gmail.com>
2023-10-11 19:28:35 +0300

    refactor: rename method

--- backend/src/core/utils.py
+++ backend/src/core/utils.py
@@ -3,7 +3,7 @@ import hashlib
 import time
 
 
-def get_code() -> str:
+def get_hash() -> str:
     """ Get 20 character long code """
     h = hashlib.sha512()
     h.update(str(time.time()).encode('utf-8'))
