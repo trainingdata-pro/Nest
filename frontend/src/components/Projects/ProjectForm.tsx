342ff54a03cf71be8af29c041c8155bb2dc296e6
Ivan <issemenov99@mail.ru>
2023-10-12 17:19:28 +0300

    fix project form

--- frontend/src/components/Projects/ProjectForm.tsx
+++ frontend/src/components/Projects/ProjectForm.tsx
@@ -13,6 +13,9 @@ import MyLabel from "../UI/MyLabel";
 import Error from "../UI/Error";
 import {useMutation, useQuery, useQueryClient} from "react-query";
 import {AxiosError} from "axios";
+import {errorNotification, successNotification} from "../UI/Notify";
+import CreatableSelect from "react-select/creatable";
+import MyButton from "../UI/MyButton";
 
 export interface SelectProps {
     value: string | number,
@@ -61,7 +64,7 @@ const ProjectForm = ({projectId, closeSidebar}: {
         onSuccess: (data) => {
             if (data) {
                 setValue('name', data.name)
-                setValue('manager', data.manager.length !==0 ? data.manager.map(manager => manager.id): (store.user_data.is_teamlead ? [] : [store.user_id]))
+                setValue('manager', data.manager.length !== 0 ? data.manager.map(manager => manager.id) : (store.user_data.is_teamlead ? [] : [store.user_id]))
                 setValue('speed_per_hour', data.speed_per_hour)
                 setValue('price_for_assessor', data.price_for_assessor)
                 setValue('price_for_costumer', data.price_for_costumer)
@@ -72,7 +75,6 @@ const ProjectForm = ({projectId, closeSidebar}: {
                 setValue('tag', data.tag.map(tag => tag.id))
                 setValue('date_of_creation', data.date_of_creation)
                 setValue('status', data.status)
-                console.log(data.tag)
                 setCurrentTags(data.tag.map(tag => {
                     return tag.id
                 }))
@@ -112,16 +114,28 @@ const ProjectForm = ({projectId, closeSidebar}: {
     const patchProject = useMutation((params: MyParams) => ProjectService.patchProject(params.id, params.data), {
         onSuccess: () => {
             queryClient.invalidateQueries('projects');
+            successNotification('Проект успешно обновлен')
         },
+        onError: (error: any) => {
+            const jsonError = JSON.parse(error.request.responseText)
+            errorNotification(jsonError[Object.keys(jsonError)[0]][0])
+        }
     });
     const postProject = useMutation((data: any) => ProjectService.postProject(data), {
-        onSuccess: () => queryClient.invalidateQueries('projects'),
-        onError: (error:AxiosError) => console.log(error.request.response)
+        onSuccess: () => {
+            queryClient.invalidateQueries('projects')
+            successNotification('Проект успешно создан')
+        },
+        onError: (error: any) => {
+            const jsonError = JSON.parse(error.request.responseText)
+            errorNotification(jsonError[Object.keys(jsonError)[0]][0])
+        }
     });
     const [tagsList, setTagsList] = useState<any[]>([])
     const [currentTags, setCurrentTags] = useState<number[]>([])
 
     const [currentStatus, setCurrentStatus] = useState<string>()
+
     function onSubmit() {
         const formValue = getValues()
         if (projectId !== 0) {
@@ -133,24 +147,39 @@ const ProjectForm = ({projectId, closeSidebar}: {
         }
     }
 
-    const onTagChange = (newValue:any) => {
-        const tagsId = newValue.map((value:any) => value.value)
+    const onTagChange = (newValue: any) => {
+        const tagsId = newValue.map((value: any) => value.value)
         setCurrentTags(tagsId)
         setValue('tag', tagsId)
     }
     const getValueTag = () => {
-        if (currentTags){
-            return tagsList.filter((tag:any) => currentTags.indexOf(tag.value) >= 0)
+        if (currentTags) {
+            return tagsList.filter((tag: any) => currentTags.indexOf(tag.value) >= 0)
         } else {
             return []
         }
     }
-    const onChangeStatus = (newValue:any) => {
+    const onChangeRegularity = (newValue: any) => {
+        setRegularity(newValue.value)
+        setValue('unloading_regularity', newValue.value)
+
+    }
+    const [regularity, setRegularity] = useState()
+    const onChangeStatus = (newValue: any) => {
         setCurrentStatus(newValue.value)
         setValue('status', newValue.value)
 
     }
-    const getValueStatus = ()=> {
+    const getRegularity = () => {
+        return regularity ? regOptions.find(s => s.value === regularity) : ''
+    }
+    const regOptions = [{label: 'Разовая', value: 'Разовая'},
+        {label: 'Будние дни', value: 'Будние дни'},
+        {label: 'Ежедневно', value: 'Ежедневно'},
+        {label: 'Раз в неделю', value: 'Раз в неделю'},
+        {label: 'Раз в две недели', value: 'Раз в две недели'},
+        {label: 'Раз в месяц', value: 'Раз в месяц'},]
+    const getValueStatus = () => {
         return currentStatus ? statusList.find(s => s.value === currentStatus) : ''
     }
     return (
@@ -162,7 +191,7 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <Error>{errors.name && errors.name?.message}</Error>
                     </div>
 
-                    <MyInput placeholder="Название проекта"
+                    <MyInput placeholder="Название проекта" className={'pl-[20px]'}
                              register={{...register('name', {required: 'Обязательное поле'})}}
                              type="text"/>
                 </FormSection>
@@ -171,7 +200,8 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <MyLabel required={true}>Asana ID: </MyLabel>
                         <Error>{errors.status && errors.status?.message}</Error>
                     </div>
-                    <MyInput placeholder="Asana ID" type="text" register={{...register('asana_id', {required:'Обязательное поле'})}}/>
+                    <MyInput className={'pl-[20px]'} placeholder="Asana ID" type="text"
+                             register={{...register('asana_id', {required: 'Обязательное поле'})}}/>
                 </FormSection>
                 {store.user_data.is_teamlead && <FormSection>
                     <div className="flex justify-between">
@@ -195,12 +225,14 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <MyLabel required={false}>Скорость в час: </MyLabel>
                         <Error>{errors.speed_per_hour && errors.speed_per_hour?.message}</Error>
                     </div>
-                    <MyInput placeholder="Скорость в час" type="text" register={{...register('speed_per_hour', {
-                        pattern:{
-                            value: /^-?\d+(\.\d+)?$/,
-                            message: 'Указано неверное значение'
-                        }
-                        })}}/>
+                    <MyInput className={'pl-[20px]'} placeholder="Скорость в час" type="text" register={{
+                        ...register('speed_per_hour', {
+                            pattern: {
+                                value: /^-?\d+(\.\d+)?$/,
+                                message: 'Указано неверное значение'
+                            }
+                        })
+                    }}/>
                 </FormSection>
 
                 <FormSection>
@@ -208,7 +240,7 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <MyLabel required={false}>Цена за единицу для асессора: </MyLabel>
                         <Error>{errors.speed_per_hour && errors.speed_per_hour?.message}</Error>
                     </div>
-                    <MyInput placeholder="Цена за единицу для асессора" type="text"
+                    <MyInput className={'pl-[20px]'} placeholder="Цена за единицу для асессора" type="text"
                              register={{...register('price_for_assessor')}}/>
                 </FormSection>
 
@@ -218,35 +250,40 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <Error>{errors.speed_per_hour && errors.speed_per_hour?.message}</Error>
 
                     </div>
-                    <MyInput placeholder="Цена за единицу для заказчика" type="text"
-                             register={{...register('price_for_costumer',{
-                                 pattern:{
-                                 value: /^-?\d+(\.\d+)?$/,
-                                 message: 'Указано неверное значение'
-                             }
-                             })}}/>
+                    <MyInput className={'pl-[20px]'} placeholder="Цена за единицу для заказчика" type="text"
+                             register={{
+                                 ...register('price_for_costumer', {
+                                     pattern: {
+                                         value: /^-?\d+(\.\d+)?$/,
+                                         message: 'Указано неверное значение'
+                                     }
+                                 })
+                             }}/>
                 </FormSection>
 
                 <FormSection>
                     <div className="flex justify-between">
                         <MyLabel required={false}>Объем выгрузок: </MyLabel>
                     </div>
-                    <MyInput placeholder="Объем выгрузок" type="text" register={{...register('unloading_value',{
-                        pattern:{
-                        value: /^-?\d+(\.\d+)?$/,
-                        message: 'Указано неверное значение'
-                    }
-                    })}}/>
+                    <MyInput className={'pl-[20px]'} placeholder="Объем выгрузок" type="text" register={{
+                        ...register('unloading_value', {
+                            pattern: {
+                                value: /^-?\d+(\.\d+)?$/,
+                                message: 'Указано неверное значение'
+                            }
+                        })
+                    }}/>
                 </FormSection>
-
-
-
                 <FormSection>
                     <div className="flex justify-between">
                         <MyLabel required={false}>Регулярность выгрузок: </MyLabel>
                     </div>
-                    <MyInput placeholder="Регулярность выгрузок" type="text"
-                             register={{...register("unloading_regularity")}}/>
+                    <CreatableSelect
+                        {...register("unloading_regularity")}
+                        options={regOptions}
+                        onChange={onChangeRegularity}
+                        value={getRegularity()}
+                    />
                 </FormSection>
 
                 <FormSection>
@@ -254,17 +291,14 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <MyLabel required={true}>Статус: </MyLabel>
                         <Error>{errors.status && errors.status?.message}</Error>
                     </div>
-                    <div className=''>
                     <Select
-                        {...register('status',{
+                        {...register('status', {
                             required: 'Обязательное поле'
                         })}
                         options={statusList}
-                        isSearchable={false}
                         value={getValueStatus()}
                         onChange={onChangeStatus}
                     />
-                    </div>
                 </FormSection>
                 <FormSection>
                     <div className="flex justify-between">
@@ -285,13 +319,12 @@ const ProjectForm = ({projectId, closeSidebar}: {
                         <MyLabel required={true}>Дата старта проекта</MyLabel>
                         <Error>{errors.date_of_creation && errors.date_of_creation?.message}</Error>
                     </div>
-                    <MyInput placeholder="Дата старта проекта"
+                    <MyInput className={'pl-[20px]'} placeholder="Дата старта проекта"
                              register={{...register('date_of_creation', {required: true})}} type="text"/>
                 </FormSection>
 
-                <button type="submit"
-                        className="bg-[#5970F6] w-full text-white rounded-md mt-2 pt-2 py-2">{projectId === 0 ? 'Добавить' : 'Сохранить'}
-                </button>
+                <MyButton>{projectId === 0 ? 'Добавить' : 'Сохранить'}
+                </MyButton>
 
             </form>
         </div>
