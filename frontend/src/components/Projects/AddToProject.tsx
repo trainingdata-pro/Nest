3b3cad94e15d4599de09d9a8c38ebf4c2116cb98
Ivan <issemenov99@mail.ru>
2023-10-16 16:57:28 +0300

    fix assessor status on project

--- frontend/src/components/Projects/AddToProject.tsx
+++ frontend/src/components/Projects/AddToProject.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import MyButton from "../UI/MyButton";
 
 const AddToProject = () => {
     return (
-        <div>
-            <div>
+        <>
+            <h2>Добавить на проект</h2>
+        <div className='flex flex-col space-y-2'>
+            <MyButton>
                 <a href="/assessors/free_resources/" target="_blank">Из свободных ресурсов</a>
-            </div>
-            <div>
-                <a href="/dashboard/assessors/?sorted=asc" target="_blank">Из моих асессоров</a>
-            </div>
+            </MyButton>
+            <MyButton>
+                <a href="/dashboard/assessors/my/?sorted=asc" target="_blank">Из моих асессоров</a>
+            </MyButton>
         </div>
+        </>
     );
 };
 
