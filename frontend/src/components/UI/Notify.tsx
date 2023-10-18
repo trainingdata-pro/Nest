48e29f2398011849edcfd9af75d3662c2d5194fe
Ivan <issemenov99@mail.ru>
2023-10-18 18:07:04 +0300

    add notify jsx

--- frontend/src/components/UI/Notify.tsx
+++ frontend/src/components/UI/Notify.tsx
@@ -1,10 +1,11 @@
 import {toast} from "react-toastify";
+import React from "react";
 const options = {
     position: toast.POSITION.BOTTOM_LEFT,
     autoClose: 5000,
     pauseOnHover: false,
     draggable: true,
 }
-export const errorNotification = (msg:string) => toast.error(msg, options);
-export const successNotification = (msg:string) => toast.success(msg, options);
-export const warnNotification = (msg:string) => toast.warn(msg, options);
+export const errorNotification = (msg:string | React.ReactNode) => toast.error(msg, options);
+export const successNotification = (msg:string | React.ReactNode) => toast.success(msg, options);
+export const warnNotification = (msg:string | React.ReactNode) => toast.warn(msg, options);
