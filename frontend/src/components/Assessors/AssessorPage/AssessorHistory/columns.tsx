b7bab40e6b8126a15483700391af3e2f289f9d12
Ivan <issemenov99@mail.ru>
2023-11-01 14:31:38 +0300

    style history table

--- frontend/src/components/Assessors/AssessorPage/AssessorHistory/columns.tsx
+++ frontend/src/components/Assessors/AssessorPage/AssessorHistory/columns.tsx
@@ -1,22 +1,23 @@
 import {createColumnHelper} from "@tanstack/react-table";
 import {IHistory} from "../../../../models/AssessorResponse";
+import React from "react";
 
 const action = {
-    'created' : 'Создать исполнителя',
-    'to_team' : 'Забрать в команду',
-    'rent' : 'Арендовать',
-    'add_project' : 'Добавить проект',
-    'remove_project' : 'Удалить с проекта',
-    'complete_project' : 'Завершить проект',
-    'left' : 'Уволить',
-    'unpin' : 'Открепить от себя',
-    'add_to_free_resource' : 'Отправить в свободные ресурсы',
-    'return_from_free_resource' : 'Вернуть из свободных ресурсов',
-    'to_vacation' : 'Отправить в отпуск',
+    'created': 'Создать исполнителя',
+    'to_team': 'Забрать в команду',
+    'rent': 'Арендовать',
+    'add_project': 'Добавить проект',
+    'remove_project': 'Удалить с проекта',
+    'complete_project': 'Завершить проект',
+    'left': 'Уволить',
+    'unpin': 'Открепить от себя',
+    'add_to_free_resource': 'Отправить в свободные ресурсы',
+    'return_from_free_resource': 'Вернуть из свободных ресурсов',
+    'to_vacation': 'Отправить в отпуск',
     'from_vacation': 'Вернуть из отпуска'
 }
 const attribute = {
-    'full_name': 'ФИО' ,
+    'full_name': 'ФИО',
     'username': 'Никнейм в Telegram',
     'manager': 'Руководитель',
     'project': 'Проект',
@@ -34,10 +35,14 @@ export const useHistoryColumns = () => {
             cell: info => attribute[info.getValue()]
         }),
         columnHelper.accessor('old_value', {
-            header: 'Старое значение'
+            header: 'Старое значение',
+            cell: (info) => <div className="cursor-pointer h-full w-full text-center break-all"><p
+                className='hover:border-b hover:border-black w-fit mx-auto'>{info.getValue()}</p></div>
         }),
         columnHelper.accessor('new_value', {
-            header: 'Новое значение'
+            header: 'Новое значение',
+            cell: (info) => <div className="cursor-pointer h-full w-full text-center break-all"><p
+                className='hover:border-b hover:border-black w-fit mx-auto'>{info.getValue()}</p></div>
         }),
         columnHelper.accessor('action', {
             header: 'Действие',
