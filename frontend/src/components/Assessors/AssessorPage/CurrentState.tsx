e3d005466ff621528da4c680ac840b1c803cf96c
Ivan <issemenov99@mail.ru>
2023-11-01 18:58:46 +0300

    fix: fix current state

--- frontend/src/components/Assessors/AssessorPage/CurrentState.tsx
+++ frontend/src/components/Assessors/AssessorPage/CurrentState.tsx
@@ -7,23 +7,23 @@ const CurrentState = ({assessorId, vacationDate}: {
     vacationDate: string | undefined | null
 }) => {
     const {data} = useQuery(['assessorHistory', assessorId], () => AssessorService.fetchAssessorHistory(assessorId, 'state'), {})
-        return (
-            <table className='border border-black'>
-                <thead>
-                    <tr className="bg-[#E7EAFF]">
-                        <th className="border-r dark:border-neutral-500 px-[2px] py-[2px]">Статус</th>
-                        <th className="border-r dark:border-neutral-500 px-[5px] py-[20px]">Дата Присвоения статуса</th>
-                        <th className="px-[5px] py-[20px]">Предположительная дата завершения статуса</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="text-center border-t dark:border-neutral-500">
-                        <td className="whitespace-nowrap border-r dark:border-neutral-500 px-[5px] py-[2px]">{data && data.results[0]?.new_value}</td>
-                        <td className="whitespace-nowrap border-r dark:border-neutral-500 px-[5px] py-[2px]">{data && data.results[0]?.timestamp.split('T')[0]}</td>
-                        <td className="whitespace-nowrap px-[5px] py-[2px]">{vacationDate && vacationDate}</td>
-                    </tr>
-                </tbody>
-            </table>
+    return (
+        <table className='border border-black'>
+            <thead>
+            <tr className="bg-[#E7EAFF]">
+                <th className="border-r dark:border-neutral-500 px-[2px] py-[2px]">Статус</th>
+                <th className="border-r dark:border-neutral-500 px-[5px] py-[20px]">Дата Присвоения статуса</th>
+                <th className="px-[5px] py-[20px]">Предположительная дата завершения статуса</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr className="text-center border-t dark:border-neutral-500">
+                <td className="whitespace-nowrap border-r dark:border-neutral-500 px-[5px] py-[2px]">{data && data.results[0]?.new_value}</td>
+                <td className="whitespace-nowrap border-r dark:border-neutral-500 px-[5px] py-[2px]">{data && data.results[0]?.timestamp.split('T')[0]}</td>
+                <td className="whitespace-nowrap px-[5px] py-[2px]">{vacationDate && vacationDate}</td>
+            </tr>
+            </tbody>
+        </table>
     );
 };
 
