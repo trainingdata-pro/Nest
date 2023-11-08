7be95631f08db7935efac7bfa9cd7081597f9b4a
Ivan <issemenov99@mail.ru>
2023-11-08 14:33:19 +0300

    add: add select page limit

--- frontend/src/components/Projects/Projects/MainPage.tsx
+++ frontend/src/components/Projects/Projects/MainPage.tsx
@@ -1,48 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Header from "../../Header/Header";
-import {
-    createColumnHelper,
-    getCoreRowModel,
-    getPaginationRowModel, getSortedRowModel,
-    SortingState,
-    useReactTable
-} from "@tanstack/react-table";
-import {Project} from "../../../models/ProjectResponse";
-import {useNavigate} from "react-router-dom";
-import {useQuery} from "react-query";
-import ProjectService from "../../../services/ProjectService";
 import Loader from "../../UI/Loader";
 import Dialog from "../../UI/Dialog";
 import ProjectForm from "../ProjectForm/ProjectForm";
 import MyButton from "../../UI/MyButton";
 import Table from "../../UI/Table";
 import {useProjectsSorting} from "./columns";
-import TablePagination from "../../UI/TablePagination";
+import {useFetchProjects} from "./queries";
 
 const MainPage = () => {
     const {getSortingString,projectsId, sorting, setShowSidebar, setProjectId, columns, showSidebar} = useProjectsSorting()
-    const [currentPage, setCurrentPage] = useState(1)
-    const [totalPages, setTotalPages] = useState(1)
-    const [totalRows, setTotalRows] = useState<number>(0)
-    const {
-        data,
-        isLoading,
-        isSuccess
-    } = useQuery(['projects', sorting, currentPage], () => ProjectService.fetchProjects(currentPage, getSortingString()), {
-        keepPreviousData: true,
-        onSuccess: data => {
-            setTotalRows(data.count)
-            setTotalPages(Math.ceil(data.count / 10))
-        }
-    })
-
-    const table = useReactTable({
-        data: isSuccess ? data.results : [],
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-        enableSorting:false,
-    })
-    if (isLoading) {
+    const {projects, currentPage, setCurrentPage, totalPages, totalRows, pageLimit, setPageLimit} = useFetchProjects({sorting: sorting, sortingString: getSortingString()})
+    if (projects.isLoading) {
         return <Loader/>
     }
 
@@ -55,11 +24,10 @@ const MainPage = () => {
                                  closeSidebar={setShowSidebar}/>
                 </Dialog>
                 <div className="h-full w-full px-8">
-
                     <div className="flex-col items-center">
                         <div className="flex justify-between my-2">
                             <div className='my-auto'>
-                                <p>Всего проектов: {data?.results.length}</p>
+                                <p>Всего проектов: {projects.data?.count}</p>
                             </div>
                             <MyButton onClick={() => {
                                 setProjectId(0)
@@ -68,8 +36,9 @@ const MainPage = () => {
                             </MyButton>
                         </div>
                         <div className='rounded-[20px] bg-white overflow-hidden overflow-x-auto'>
-                        <Table table={table}/>
-                        <TablePagination totalRows={totalRows} currentPage={currentPage} totalPages={totalPages} setCurrentPage={setCurrentPage}/>
+                        <Table data={projects.isSuccess ? projects.data.results : []} columns={columns} totalRows={totalRows} currentPage={currentPage} totalPages={totalPages}
+                               setCurrentPage={setCurrentPage} pageLimit={pageLimit} setPageLimit={setPageLimit} pages={true}/>
+
                         </div></div>
                 </div>
             </div>
