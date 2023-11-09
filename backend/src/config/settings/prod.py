8490175e9184906b4597ca37b15acd5bd998c7cc
DmBand <d.bandysik@gmail.com>
2023-11-09 15:17:47 +0300

    chore: fix prod allowed hosts

--- backend/src/config/settings/prod.py
+++ backend/src/config/settings/prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 MAIN_HOST = 'https://assessors-test.trainingdata.solutions'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.trainingdata.solutions']
 
 CORS_ALLOWED_ORIGINS = [MAIN_HOST]
 CORS_ALLOW_CREDENTIALS = True
