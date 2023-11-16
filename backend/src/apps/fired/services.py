38067b035dd9285f2f5b3a31ccd46e4591ec5f9c
DmBand <d.bandysik@gmail.com>
2023-11-16 17:33:35 +0300

    fix: move the change of assessor's status after dismissal to another service

--- backend/src/apps/fired/services.py
+++ backend/src/apps/fired/services.py
@@ -3,54 +3,33 @@ from typing import Optional
 
 from apps.assessors.models import Assessor, AssessorState
 from apps.fired.models import Fired, BlackList
+from core.mixins import BaseService
 
 
-class FiredService:
+class FiredService(BaseService):
     model = Fired
 
     def fire(self, assessor: Assessor, reason: str, possible_return_date: Optional[datetime.date] = None) -> Fired:
-        fired = self.__create_instance(
+        fired = self.create_instance(
             assessor=assessor,
             reason=reason,
             possible_return_date=possible_return_date
         )
-        assessor.state = AssessorState.FIRED
-        return self.__perform_save(fired)
-
-    def __create_instance(self, **kwargs) -> Fired:
-        return self.model(**kwargs)
-
-    @staticmethod
-    def __perform_save(instance: Fired) -> Fired:
-        instance.save()
-        return instance
+        return self.perform_save(fired)
 
 
-class BlackListService:
+class BlackListService(BaseService):
     model = BlackList
 
     def blacklist(self, assessor: Assessor, reason: str) -> BlackList:
-        blacklist = self.__create_instance(
+        blacklist = self.create_instance(
             assessor=assessor,
             reason=reason
         )
-        assessor.state = AssessorState.BLACKLIST
-        return self.__perform_save(blacklist)
+        return self.perform_save(blacklist)
 
     def remove_item(self, instance: BlackList) -> None:
-        return self.__perform_delete(instance)
-
-    def __create_instance(self, **kwargs) -> BlackList:
-        return self.model(**kwargs)
-
-    @staticmethod
-    def __perform_save(instance: BlackList) -> BlackList:
-        instance.save()
-        return instance
-
-    @staticmethod
-    def __perform_delete(instance: BlackList) -> None:
-        instance.delete()
+        return self.perform_delete(instance)
 
 
 fired_service = FiredService()
