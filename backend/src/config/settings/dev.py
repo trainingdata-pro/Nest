8aa1b0859ac2bad0c4e3b372ac282144d4418bdb
DmBand <d.bandysik@gmail.com>
2023-11-16 18:53:48 +0300

    chore: use redis sentinel and db claster in production

--- backend/src/config/settings/dev.py
+++ backend/src/config/settings/dev.py
@@ -7,6 +7,17 @@ MAIN_HOST = 'http://localhost:3000'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
+    }
+}
+
 SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(days=1)
 
 CORS_ALLOWED_ORIGINS = [MAIN_HOST]
