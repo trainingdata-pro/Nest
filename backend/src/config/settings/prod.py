312ad3e2854bd120decfad3a79cef90f6a3974fe
DmBand <d.bandysik@gmail.com>
2023-11-21 17:11:43 +0300

    chore: change main host

--- backend/src/config/settings/prod.py
+++ backend/src/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-MAIN_HOST = 'https://assessors-test.trainingdata.solutions'
+MAIN_HOST = 'https://nest.trainingdata.solutions'
 
 ALLOWED_HOSTS = ['.trainingdata.solutions']
 
