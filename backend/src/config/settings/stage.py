549f7e2423f995cbf253d6a4d53ec7cc9de75117
DmBand <d.bandysik@gmail.com>
2023-11-29 17:57:06 +0300

    chore: add stage config

---
+++ backend/src/config/settings/stage.py
@@ -0,0 +1,14 @@
+from .prod import *
+
+MAIN_HOST = 'https://assessors-test.trainingdata.solutions'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
+    }
+}
