e928e74edb9dd6f4dec4ec286bd397229c8a9f1e
DmBand <d.bandysik@gmail.com>
2023-12-05 11:30:58 +0300

    chore: fix stage config

--- backend/src/config/settings/stage.py
+++ backend/src/config/settings/stage.py
@@ -1,6 +1,10 @@
-from .prod import *
+from .base import *
+
+DEBUG = False
 
 MAIN_HOST = 'https://assessors-test.trainingdata.solutions'
+
+ALLOWED_HOSTS = ['.trainingdata.solutions']
 
 DATABASES = {
     'default': {
@@ -12,3 +16,8 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
+
+CORS_ALLOWED_ORIGINS = [MAIN_HOST]
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [MAIN_HOST]
