d14189d754a4babb3961092054c1cda2cb45f921
DmBand <d.bandysik@gmail.com>
2023-12-19 16:11:31 +0300

    chore: use claster db

--- backend/src/config/settings/stage.py
+++ backend/src/config/settings/stage.py
@@ -13,7 +13,11 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
-        'PORT': os.getenv('POSTGRES_PORT', 5432)
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
+        'OPTIONS': {
+            'sslmode': os.environ.get('SSL_MODE'),
+            'target_session_attrs': os.environ.get('TARGET_SESSION_ATTRS')
+        }
     }
 }
 
